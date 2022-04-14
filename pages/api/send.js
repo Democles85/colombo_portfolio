23061d4bf7f03ab5a204b890cc9a47ed253112ee
Sixhei Tartari <democleschannel28@gmail.com>
2022-04-14 20:54:12 +0200

    Finished Email Sender

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,21 +1,25 @@
 import sgMail from '@sendgrid/mail'
-import 'dotenv/config'
+require('dotenv').config
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const msg = {
-  to: 'democleschannel28@gmail.com',
-  from: 'resonatewithyourself@gmail.com',
-  subject: 'Test Message',
-  text: 'Test Text',
-  html: '<strong>Test Strong Text</strong>'
+async function sendEmail(req, res) {
+  try {
+    await sgMail.send({
+      to: 'resonatewithyourself@gmail.com',
+      from: 'resonatewithyourselfemailer@gmail.com',
+      subject: `[Lead from website] : ${req.body.subject}`,
+      html: `  
+        <h3>You've got a new mail from ${req.body.firstName}, their email is: ${req.body.email} </h3>
+        <div style="font-size: 16px;">
+        <strong>Message:</strong>
+        <p>${req.body.message}</p>
+        </div>`
+    })
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ error: error.message })
+  }
+  return res.status(200).json({ error: '' })
 }
 
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('email sent')
-  })
-  .catch(error => {
-    console.log('There was an error', error)
-  })
+export default sendEmail
--- pages/contact.js
+++ pages/contact.js
@@ -2,12 +2,13 @@ import {
   Box,
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
   Input,
   Text,
   Textarea
 } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 
@@ -15,57 +16,175 @@ const Contact = () => {
   const formBoxPaddingY = 2.5
   const formBoxPaddingX = 25
   const formBoxWidth = '100%'
+
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+
+  const [errors, setErrors] = useState({})
+
+  const [buttonText, setButtonText] = useState('Send')
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const [showFailureMessage, setShowFailureMessage] = useState(false)
+
+  const handleValidation = () => {
+    let tempErrors = {}
+    let isValid = true
+
+    if (firstName.length <= 0) {
+      tempErrors['firstName'] = true
+      isValid = false
+    }
+
+    if (subject.length <= 0) {
+      tempErrors['subject'] = true
+      isValid = false
+    }
+
+    if (message.length <= 0) {
+      tempErrors['message'] = true
+      isValid = false
+    }
+
+    if (email.length <= 0) {
+      tempErrors['email'] = true
+      isValid = false
+    }
+
+    setErrors({ ...tempErrors })
+    console.log('errors', errors)
+    return isValid
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    let isValidForm = handleValidation()
+
+    if (isValidForm) {
+      setButtonText('Sending')
+
+      const res = await fetch('/api/send', {
+        body: JSON.stringify({
+          email: email,
+          firstName: firstName,
+          subject: subject,
+          message: message
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      })
+
+      const { error } = await res.json()
+      if (error) {
+        console.log(error)
+        setShowSuccessMessage(false)
+        setShowSuccessMessage(true)
+        setButtonText('Send')
+        return
+      }
+      console.log(firstName, email, subject, message)
+    }
+  }
+
   return (
     <Layout title="Contact">
       <Section delay={0.2}>
         <Box>
-          <FormControl>
-            <Box display="flex" flexDir={['column', null, 'row']}>
-              <Box
-                width={formBoxWidth}
-                py={formBoxPaddingY}
-                px={formBoxPaddingX}
-              >
-                <FormLabel htmlFor="first-name">First Name</FormLabel>
-                <Input id="first-name" placeHolder="First Name" isRequired />
-              </Box>
-              <Box
+          <form onSubmit={handleSubmit}>
+            <FormControl>
+              <Box display="flex" flexDir={['column', null, 'row']}>
+                <Box
+                  width={formBoxWidth}
+                  py={formBoxPaddingY}
+                  px={formBoxPaddingX}
+                >
+                  <FormLabel htmlFor="first-name">First Name</FormLabel>
+                  <Input
+                    id="first-name"
+                    placeHolder="First Name"
+                    isRequired
+                    value={firstName}
+                    onChange={e => {
+                      setFirstName(e.target.value)
+                    }}
+                  />
+                </Box>
+                {/* <Box
                 width={formBoxWidth}
                 py={formBoxPaddingY}
                 px={formBoxPaddingX}
               >
                 <FormLabel htmlFor="last-name">Last Name</FormLabel>
                 <Input id="last-name" placeHolder="Last Name" />
+                <FormHelperText>Last Name is not required</FormHelperText>
+              </Box> */}
               </Box>
-            </Box>
-            <Box width={formBoxWidth} py={formBoxPaddingY} px={formBoxPaddingX}>
-              <FormLabel htmlFor="subject">Subject</FormLabel>
-              <Input id="subject" placeHolder="Subject" isRequired />
-            </Box>
-            <Box width={formBoxWidth} py={formBoxPaddingY} px={formBoxPaddingX}>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input id="email" placeHolder="Email" isRequired />
-            </Box>
-            <Box width={formBoxWidth} py={formBoxPaddingY} px={formBoxPaddingX}>
-              <FormLabel htmlFor="message">Message</FormLabel>
-              <Textarea
-                placeholder="Type your message"
-                isRequired
-                isFullWidth
-                id="message"
-              />
-            </Box>
-            <Box
-              width="100%"
-              display="flex"
-              justifyContent="center"
-              py={formBoxPaddingY}
-            >
-              <Button width="50%" type="submit">
-                Submit
-              </Button>
-            </Box>
-          </FormControl>
+              <Box
+                width={formBoxWidth}
+                py={formBoxPaddingY}
+                px={formBoxPaddingX}
+              >
+                <FormLabel htmlFor="subject">Subject</FormLabel>
+                <Input
+                  id="subject"
+                  placeHolder="Subject"
+                  isRequired
+                  value={subject}
+                  onChange={e => {
+                    setSubject(e.target.value)
+                  }}
+                />
+              </Box>
+              <Box
+                width={formBoxWidth}
+                py={formBoxPaddingY}
+                px={formBoxPaddingX}
+              >
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  id="email"
+                  placeHolder="Email"
+                  isRequired
+                  value={email}
+                  onChange={e => {
+                    setEmail(e.target.value)
+                  }}
+                />
+              </Box>
+              <Box
+                width={formBoxWidth}
+                py={formBoxPaddingY}
+                px={formBoxPaddingX}
+              >
+                <FormLabel htmlFor="message">Message</FormLabel>
+                <Textarea
+                  placeholder="Type your message"
+                  isRequired
+                  isFullWidth
+                  id="message"
+                  value={message}
+                  onChange={e => {
+                    setMessage(e.target.value)
+                  }}
+                />
+              </Box>
+              <Box
+                width="100%"
+                display="flex"
+                justifyContent="center"
+                py={formBoxPaddingY}
+              >
+                <Button width="50%" type="submit">
+                  Submit
+                </Button>
+              </Box>
+            </FormControl>
+          </form>
         </Box>
       </Section>
     </Layout>
