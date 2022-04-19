f5ab5381bcdc596067fd23b02171981014421e59
Sixhei Tartari <democleschannel28@gmail.com>
2022-04-19 22:04:21 +0200

    Updated Website

--- pages/services.js
+++ pages/services.js
@@ -7,6 +7,7 @@ import thumbBodywork from '../public/images/works/thumb_1.jpeg'
 import thumbAuthenticTantra from '../public/images/works/thumb_2.jpeg'
 import thumbDeArmouring from '../public/images/works/thumb_3.jpeg'
 import thumbSoundHealing from '../public/images/works/thumb_4.jpeg'
+import thumbAdvancedEnergeticBodywork from '../public/images/works/thumb_5.jpeg'
 import thumbComingSoon from '../public/images/works/coming_soon_thumb.png'
 import thumbDemoclesNews from '../public/images/works/democles_news_thumnail.png'
 import thumbMathsGame from '../public/images/works/maths_game_thumbnail.png'
@@ -66,6 +67,26 @@ const Services = () => (
               id="sound-healing"
               title="Sound Healing"
               thumbnail={thumbSoundHealing}
+            >
+              Sound has the ability to activate parts of the subconscious brain,
+              which can give access to unconscious memories...
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Section>
+
+      <Section delay={0.6}>
+        <Divider my={6} />
+
+        <Heading as="h3" fontSize={20} mb={4}>
+          Advanced Energetic Bodywork
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="sound-healing"
+              title="Sound Healing"
+              thumbnail={thumbAdvancedEnergeticBodywork}
             >
               Sound has the ability to activate parts of the subconscious brain,
               which can give access to unconscious memories...
